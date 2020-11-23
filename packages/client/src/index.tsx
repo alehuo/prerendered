adfc5d3a6c2b30d6c73449cfbd3e783401b343c4
alehuo <aleksi.huotala@reaktor.com>
2020-11-23 16:36:03 +0200

    Update Context in client

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,9 +4,14 @@ import { getPrerenderedData } from "@prerendered/shared";
 const data = getPrerenderedData();
 const SSRContext_ = React.createContext(data);
 
-export const SSRContext: React.FC = ({ children }) => (
-  <SSRContext_.Provider value={data}>{children}</SSRContext_.Provider>
-);
+type Props = {
+  value?: any;
+};
+
+export const SSRContext: React.FC<Props> = ({
+  children,
+  value = getPrerenderedData(),
+}) => <SSRContext_.Provider value={value}>{children}</SSRContext_.Provider>;
 
 export function useSSR<T extends object>(): T {
   return useContext(SSRContext_) as T;
