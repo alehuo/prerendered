7c28c1c46b81aeb57c86abea159c485860d04549
Aleksi Huotala <aleksi.huotala@reaktor.com>
2021-04-07 20:43:06 +0300

    Example: Update App.tsx

--- examples/express/src/App.tsx
+++ examples/express/src/App.tsx
@@ -1,3 +1,13 @@
-import React from "react"
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { SSRContext } from '@prerendered/client';
+import { Client } from './Client';
 
-export const App = () => <div>Hello world!</div>
+// You might want to use Redux or some other form of state management to keep the UI better in sync.
+
+hydrate(
+  <SSRContext>
+    <Client />
+  </SSRContext>,
+  document.getElementById('container'),
+);
